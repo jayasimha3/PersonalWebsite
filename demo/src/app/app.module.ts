bfbe3edb017a9ff0fd91dfc066411f71fbedea23
jayasimha.3 <jayasimha.3@osu.edu>
2020-05-24 21:37:38 -0500

    added routing for navbar (not projects)

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,19 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExtracurricularComponent } from './extracurricular/extracurricular.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'about',
+    pathMatch: 'full'
+  },
+  {
+    path: 'extracurricular',
+    component: ExtracurricularComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'work-experience',
+    component: WorkExperienceComponent
+  },
+  {
+    path: '',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    WorkExperienceComponent,
+    ProjectsComponent,
+    ExtracurricularComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
